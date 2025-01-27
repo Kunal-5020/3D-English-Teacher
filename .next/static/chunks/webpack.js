c765a7e47bc7c6b3eb40cc75473c3467f621e020
Kunal-5020 <kunalbhadana15@gmail.com>
2025-01-27 16:06:54 +0530

    new method

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ca5c164290d4f3e"; }
+/******/ 		__webpack_require__.h = function() { return "4e08316c3431de30"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
